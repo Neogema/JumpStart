6d7ff778f440d7a267f15e89ffbd82ca8624a683
Neogema <neogema@yahoo.com>
2017-10-18 10:54:54 -0400

    Completed site

---
+++ resources/javascript/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $('.carousel').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplayspeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+  var userScroll = 0;
+  $(window).scroll(function() {
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - userScroll > 50) {
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navHeight}, 150);
+      userScroll = scrollTop;
+    }else if (userScroll - scrollTop > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      userScroll = scrollTop;
+    }
+  });
+});
